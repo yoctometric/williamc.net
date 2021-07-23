d8446a52741ce3d09262c3f67b62bc03ad694270
yoctometric <willcunningham64@gmail.com>
2021-07-22 21:05:15 -0400

    pre-push

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+
+
+    // button interaction
+    $("#games-button-left").click(function(){
+        alert('clicked!');
+    });
+    
+    $("#games-button-right").click(function(){
+        alert('clicked!');
+    });
+});
